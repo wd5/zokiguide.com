def600c4ceebf19bdd83303cf9f3b75b4f435ec9
Dan <dan.tyan@gmail.com>
2012-11-12 20:49:00 +0600

    cleaned code

--- zokiguide/settings.py
+++ zokiguide/settings.py
@@ -106,14 +106,16 @@ TEMPLATE_LOADERS = (
  )
 
 MIDDLEWARE_CLASSES = ( 
+#    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'django.middleware.cache.FetchFromCacheMiddleware',
  )
 
 ROOT_URLCONF = 'zokiguide.urls'
