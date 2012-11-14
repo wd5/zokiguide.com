0fdc1e651c10ee9e85996dfc8d2006a37febe798
Dan <dan.tyan@gmail.com>
2012-11-14 09:45:40 +0600

    fixed pathes

--- zokiguide/settings.py
+++ zokiguide/settings.py
@@ -20,9 +20,17 @@ MANAGERS = ADMINS
 if IN_PRODUCTION:
     DOCROOT = '/home/dantyan/webapps/zokiguide/zokiguide/'
     APPPATH = '/home/dantyan/webapps/zokiguide/zokiguide/'
+    MEDIA_ROOT = '/home/dantyan/webapps/zokiguide_media/'
+    MEDIA_URL = 'http://media.zokiguide.com/'
+    STATIC_ROOT = '/home/dantyan/webapps/zokiguide_static/'
+    STATIC_URL = 'http://static.zokiguide.com/'
 else:
     DOCROOT = '/home/dan/www/django/zokiguide/'
     APPPATH = '/home/dan/www/django/zokiguide/'
+    MEDIA_ROOT = DOCROOT + 'media/'
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = DOCROOT + 'static/'
+    STATIC_URL = '/static/'
 
 
 
@@ -51,18 +59,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = DOCROOT + 'media/'
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = DOCROOT + 'static/'
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
