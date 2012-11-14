1adbb9ec6d8ed7632bcc183dacdf822148042b39
Dan <dan.tyan@gmail.com>
2012-11-14 12:46:07 +0600

    commented unused import

--- zokiguide/settings.py
+++ zokiguide/settings.py
@@ -1,5 +1,5 @@
 # Django settings for zokiguide project.
-import os
+#import os
 
 IN_PRODUCTION = True
 
@@ -11,7 +11,7 @@ except ImportError, e:
 DEBUG = not IN_PRODUCTION
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = ( 
+ADMINS = (
     ( 'Dan Tyan', 'dan.tyan@gmail.com' ),
  )
 
@@ -77,7 +77,7 @@ USE_TZ = True
 
 
 # Additional locations of static files
-STATICFILES_DIRS = ( 
+STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -98,7 +98,7 @@ STATICFILES_DIRS = (
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = ( 
+STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
@@ -107,13 +107,13 @@ STATICFILES_FINDERS = (
 
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = ( 
+TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
  )
 
-MIDDLEWARE_CLASSES = ( 
+MIDDLEWARE_CLASSES = (
 #    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
 
@@ -131,7 +131,7 @@ ROOT_URLCONF = 'zokiguide.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'zokiguide.wsgi.application'
 
-TEMPLATE_DIRS = ( 
+TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -151,7 +151,7 @@ TEMPLATE_DIRS = (
 #    os.path.join(os.path.dirname(__file__), 'templates').replace('\\', '/'),
  )
 
-TEMPLATE_CONTEXT_PROCESSORS = ( 
+TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.debug',
@@ -164,7 +164,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 #    'blog.context_processors.common',
  )
 
-INSTALLED_APPS = ( 
+INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -317,7 +317,7 @@ TINYMCE_DEFAULT_CONFIG = {
 INTERNAL_IPS = ( '127.0.0.1', )
 
 
-STATUS_CHOICES = ( 
+STATUS_CHOICES = (
     ( 'active', 'active' ),
     ( 'inactive', 'inactive' ),
     ( 'draft', 'draft' ),
