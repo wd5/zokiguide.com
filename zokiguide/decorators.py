30dece2abb583378563943a3c381e1a274640451
Dan <dan.tyan@gmail.com>
2012-12-25 01:23:43 +0600

    lang

--- zokiguide/decorators.py
+++ zokiguide/decorators.py
@@ -79,3 +79,38 @@ def render_to_json(func):
         json = simplejson.dumps(result, cls=DjangoJSONEncoder)
         return HttpResponse(json, mimetype="application/json")
     return update_wrapper(wrapper, func)
+
+
+class json_response(object):
+    def __init__(self, login_required = False, ajax_required = False):
+        self.login_required = login_required
+        self.ajax_required = ajax_required
+    def __call__(self, func):
+        class_args = self
+        def decorator(request, *args, **kwargs):
+            if class_args.login_required and not request.user.is_authenticated():
+                objects = {
+                    "status": "error",
+                    "msg": "User not authenticated"
+                }
+            elif class_args.ajax_required and not request.is_ajax():
+                objects = {
+                    "status": "error",
+                    "msg": "Request gone wrong :("
+                }
+            else:
+                objects = func(request, *args, **kwargs)
+
+            if isinstance(objects, HttpResponse):
+                return objects
+            try:
+                data = simplejson.dumps(objects)
+                if 'callback' in request.REQUEST:
+                    # a jsonp response!
+                    data = '%s(%s);' % (request.REQUEST['callback'], data)
+                    return HttpResponse(data, "text/javascript")
+            except:
+                data = simplejson.dumps(str(objects))
+            return HttpResponse(data, "application/json")
+
+        return decorator
